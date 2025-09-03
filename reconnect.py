5fe7411fca02a7baf08726405d663f514c1af922
Bakirullit <bakirullit@gmail.com>
2025-09-03 17:20:39 +0500

    fix: run in headless mode by default

--- reconnect.py
+++ reconnect.py
@@ -25,7 +25,7 @@ logging.basicConfig(
 
 # Selenium setup
 options = Options()
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options)
